a4629ddbb10519dc424e83b61c6d349a5b77be69
IFP <ifp@CPL-MacLab22.local>
2021-06-15 10:52:25 -0400

    this commit supports designsprint.html

--- tools_for_curiousity_app/t4c/urls.py
+++ tools_for_curiousity_app/t4c/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('minigame2/', views.minigame2, name='minigame2'),
     path('minigame3/', views.minigame3, name='minigame3'),
     path('minigame4/', views.minigame4, name='minigame4'),
+    path('designsprint/', views.designsprint, name='designsprint'),
 ]
